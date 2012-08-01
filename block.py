aee3f249c2839f50a92e467bce583c319427ca47
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2012-08-01 19:24:02 -0400

    Fleshed out the Block
    -Blocks now appear on a grid via the mouse button
    -Blocks now can take on colors
    -Added a Block sprite to sprite.png

--- block.py
+++ block.py
@@ -1,9 +1,27 @@
+import math
+
 import pygame
 
+import config
+import ingame
 import gameobject
 
+STATES = config.Enum('IDLE', 'APPEARING', 'ACTIVE', 'FALLING', 'DYING')
+FRAME  = pygame.Rect(0, 32, 16, 16)
+
 class Block(gameobject.GameObject):
-    def __init__(self):
-        pass
-    
-    
+    def __init__(self, pos):
+        gameobject.GameObject.__init__(self)
+        self.actions = {
+                        STATES.IDLE     : None,
+                        STATES.APPEARING: NotImplemented,
+                        STATES.ACTIVE   : NotImplemented,
+                        STATES.FALLING  : NotImplemented,
+                        STATES.DYING    : NotImplemented
+                        }
+        self.image = config.SPRITES.subsurface(FRAME).copy()  #@UndefinedVariable
+        self.rect = pygame.Rect(math.floor(pos[0]/16.0)*16, math.floor(pos[1]/16.0)*16, 16, 16)
+        self.state = STATES.ACTIVE
+        
+        self.add(ingame.BLOCKS)
+        self.image = config.blend_color(self.image, pygame.Color(0, 0, 255))
--- ingame.py
+++ ingame.py
@@ -1,5 +1,6 @@
 import pygame
 
+import block
 import collisions
 import enemysquadron
 import enemy
@@ -9,36 +10,25 @@ import shipbullet
 
 from gamestate import GameState
 
-PLAYER  = pygame.sprite.RenderUpdates()
-ENEMIES = pygame.sprite.RenderUpdates()
+PLAYER  = None
+ENEMIES = None
+BLOCKS  = pygame.sprite.RenderUpdates()
 
 class InGameState(GameState):    
     def __init__(self):
         self.collision_grid = collisions.CollisionGrid(4, 4, 1)
-        self.group_list     = [PLAYER, ENEMIES]
-        self.ship           = player.Ship()
-    
-        PLAYER.add(self.ship             )
-        ENEMIES.add(enemysquadron.enemies)
-        enemysquadron.reset()
+        self.group_list.append(BLOCKS)
     
     def events(self):
         for e in pygame.event.get():
-            if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
-                self.ship.on_fire_bullet()
+            if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                BLOCKS.add(block.Block(e.pos))        
     
     def logic(self):
-        if ENEMIES.sprites() == []:
-            enemysquadron.reset()
-        
         self.collision_grid.update()
         
         for g in self.group_list:
             g.update()
-            
-        if enemy.Enemy.should_flip == True:
-            enemy.Enemy.velocity[0] *= -1
-            enemy.Enemy.should_flip = False
     
     def render(self):
         pygame.display.get_surface().fill((0, 0, 0))
