12b1e6b8167f481ad511cb558224e3fcef622e32
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2012-08-20 19:37:11 -0400

    Blocks are now subject to gravity and collide with each other
    -Added Block.fall()

--- block.py
+++ block.py
@@ -10,6 +10,7 @@ import gameobject
 
 STATES = config.Enum('IDLE', 'APPEARING', 'ACTIVE', 'FALLING', 'DYING')
 FRAME  = pygame.Rect(0, 32, 16, 16)
+GRAVITY = .5
 
 class Block(gameobject.GameObject):
     def __init__(self, pos):
@@ -18,12 +19,37 @@ class Block(gameobject.GameObject):
                         STATES.IDLE     : None,
                         STATES.APPEARING: NotImplemented,
                         STATES.ACTIVE   : NotImplemented,
-                        STATES.FALLING  : NotImplemented,
+                        STATES.FALLING  : self.fall     ,
                         STATES.DYING    : NotImplemented
                         }
+        
+        self.acceleration[1] = GRAVITY
         self.image = config.SPRITES.subsurface(FRAME).copy()  #@UndefinedVariable
         self.rect = pygame.Rect(math.floor(pos[0]/16.0)*16, math.floor(pos[1]/16.0)*16, 16, 16)
-        self.state = STATES.ACTIVE
+        self.position = list(self.rect.topleft)
+        self.state = STATES.FALLING
         
         self.add(ingame.BLOCKS)
         self.image = color.blend_color(self.image, random.choice(color.Colors.LIST))
+        
+    def on_collide(self, other):
+        if isinstance(other, Block) and self.state == STATES.FALLING and other.state == STATES.ACTIVE:
+            self.acceleration[1] = 0
+            self.velocity[1] = 0
+            self.rect.bottom = other.rect.top
+            
+            self.state = STATES.ACTIVE
+        
+    def fall(self):
+        self.velocity[1] += self.acceleration[1]
+        self.position[1] += self.velocity[1]
+        self.rect.topleft = map(round, self.position)
+        
+        if self.rect.bottom >= config.screen.get_height() * .9:
+            self.acceleration[1] = 0
+            self.velocity[1] = 0
+            self.snap()
+            self.state = STATES.ACTIVE
+            
+    def snap(self):
+        self.rect.topleft = (math.floor(self.position[0]/16)*16, math.floor(self.position[1]/16)*16)
--- gameobject.py
+++ gameobject.py
@@ -3,11 +3,12 @@ import pygame
 class GameObject(pygame.sprite.Sprite): 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.acceleration = [0, 0]
+        self.acceleration = [0.0, 0.0]
         self.actions      = dict()
+        self.position     = [0.0, 0.0]
         self.rect         = None
         self.state        = None
-        self.velocity     = [0, 0]
+        self.velocity     = [0.0, 0.0]
         
     def on_collide(self, other):
         pass
