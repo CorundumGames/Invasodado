39479c527624afde2e667e91c1d01db307f7d086
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2012-11-15 15:15:35 -0500

    Added fullscreen mode
    -Just press F1 to toggle fullscreen.

--- __init__.py
+++ __init__.py
@@ -15,8 +15,14 @@ pygame.font.init()
 
 from core import config
 from core import gsm
+from core import settings
 
 while True:
+    if pygame.key.get_pressed()[pygame.K_F1]:
+        settings.fullscreen = not settings.fullscreen
+        config.screen = pygame.display.set_mode(settings.resolution, pygame.FULLSCREEN * settings.fullscreen)
+            
+    
     gsm.update()
     
     if pygame.event.peek(pygame.QUIT):
