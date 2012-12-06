efe470b87a86aebf213b84bf1c3c3c7a50d8fdc0
Kyle O'Connor <koconnor26@gmail.com>
2012-12-06 13:49:07 -0500

    The game can now be paused
    -Pressing p cycles pausing

--- __init__.py
+++ __init__.py
@@ -21,19 +21,27 @@ from game import mainmenu
 
 gsm.current_state = mainmenu.MainMenu()
 
-def main():
-    while True:
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_F1]:
-            settings.fullscreen = not settings.fullscreen
-            config.screen = pygame.display.set_mode(settings.resolution, (pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF) * settings.fullscreen )
-        elif keys[pygame.K_ESCAPE]:
-            break
-    
-        gsm.update()
-    
-        if pygame.event.peek(pygame.QUIT):
-            break
+pause = False
+
+while True:
+    if pygame.key.get_pressed()[pygame.K_F1]:
+        settings.fullscreen = not settings.fullscreen
+        config.screen = pygame.display.set_mode(settings.resolution, (pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF) * settings.fullscreen )
+    elif keys[pygame.K_ESCAPE]:
+        break
+
+    for event in pygame.event.get(pygame.KEYDOWN):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                print(pause)
+                pause = not pause
+        pygame.event.clear(pygame.KEYDOWN)
         
+    if not pause:
+        gsm.update()
+        
+    if pygame.event.peek(pygame.QUIT):
+        break
+    
 if __name__ == '__main__':
     main()
