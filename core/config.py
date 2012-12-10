e3d2f464dd0871fe0fd9006c0a90acb3c7e2ee54
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2012-12-09 20:31:35 -0500

    Added debug mode
    -Debug mode can now be toggled with D
    -Created functions in config.py for toggling frame limit and debug mode
    -Also cleaned up some comments
    -Moved UFO creation, block creation, and mass block clearing to debug mode
    -Moved the above from a long chain of conditionals to a dict
    -Removed unnecessary gloal declarations in ingame.py

--- core/config.py
+++ core/config.py
@@ -3,30 +3,30 @@ import pygame.image
 
 import settings
 
-#The window we blit graphics onto.
 screen = pygame.display.set_mode(settings.resolution, pygame.DOUBLEBUF)
+#The window we blit graphics onto.
 
-#A tuple of all available screen resolutions.
 SCREEN_DIMS = tuple(pygame.display.list_modes())
+#A tuple of all available screen resolutions.
 
-#How many colors we'll use
 NUM_COLORS = 5
+#How many colors we'll use for the blocks
 
 FLAGS = pygame.HWSURFACE | pygame.HWACCEL | pygame.ASYNCBLIT | pygame.RLEACCEL
 #The flags used to create all Surfaces; these are best for performance.
 
 DEPTH = screen.get_bitsize()
+#The color depth used, in bits
 
-#Screen Width and Height
 SCREEN_WIDTH  = screen.get_width()  #640 20 cells 32
 SCREEN_HEIGHT = screen.get_height() #480 15 cells 32
+#Screen width and height, in pixels
 
-#The main spritesheet for this game.
 SPRITES = pygame.image.load("./gfx/sprites.png").convert(DEPTH, FLAGS)
+#The main spritesheet for this game.
 
-#The background
 BG = pygame.image.load("./gfx/bg.png").convert(DEPTH, FLAGS)
-
+#The background
 
 FONT = pygame.font.Font("./gfx/font.ttf", 18)
 #The main typeface we will use; we might use more.
@@ -34,17 +34,23 @@ FONT = pygame.font.Font("./gfx/font.ttf", 18)
 pause = False
 #True if the game is paused
 
+limit_frame = True
+#True if we're restricting framerate to 60FPS
+
+debug = False
+#True if we're in debug mode
+
 class Enum(object):
     def __init__(self, *keys):
         self.__dict__.update(zip(keys, range(len(keys))))
 
 
-def fullScreen():
+def toggle_fullscreen():
 #Toggles fullscreen.
     settings.fullscreen = not settings.fullscreen
     screen = pygame.display.set_mode(settings.resolution, (pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF) * settings.fullscreen )
 
-def togglePause():
+def toggle_pause():
 #Handles pausing of the game...
     global pause
     pause = not pause
@@ -56,4 +62,11 @@ def togglePause():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
             #If the P key is pressed...
                 pause = not pause
-                break
+                break
+def toggle_frame_limit():
+    global limit_frame
+    limit_frame = not limit_frame
+    
+def toggle_debug():
+    global debug
+    debug = not debug
--- game/ingame.py
+++ game/ingame.py
@@ -9,7 +9,7 @@ import core.color      as color
 import core.config     as config
 import core.gamestate  as gamestate
 
-
+import balloflight
 import block
 import blockgrid
 import enemy
@@ -27,66 +27,71 @@ HUD     = pygame.sprite.LayeredUpdates()
 DEFAULT_MULTIPLIER = 10
 multiplier         = DEFAULT_MULTIPLIER
 
-global score
-global prev_score
-score = 0
+score      = 0
 prev_score = None
 
-global lives
-global prev_lives
-lives = 3
+lives      = 3
 prev_lives = None
 
 class InGameState(gamestate.GameState):    
     def __init__(self):
+        self.block_list     = []
+        #The blocks available for use
+        
         self.collision_grid = collisions.CollisionGrid(4, 4, 1)
+        #The collision-handling system
+        
         self.group_list    += [BLOCKS, ENEMIES, PLAYER, HUD]
+        #The groups of sprites to process
+        
         self.ship           = player.Ship()
+        #The player
+        
         self.ufo            = ufo.UFO()
+        #The UFO
         
-        self.hud_score       = hudobject.HudObject(pygame.Surface((0, 0)), pygame.Rect(16, 16, 0, 0))
-        self.hud_lives       = hudobject.HudObject(pygame.Surface((0, 0)), pygame.Rect(config.SCREEN_WIDTH-160, 16, 0, 0))
-        
-        self.frame_limit  = True
+        self.hud_score = hudobject.HudObject(pygame.Surface((0, 0)), pygame.Rect(16, 16, 0, 0))
+        self.hud_lives = hudobject.HudObject(pygame.Surface((0, 0)), pygame.Rect(config.SCREEN_WIDTH-160, 16, 0, 0))
+        #The components of our HUD; let the player know how he's doing!
+    
         self.game_running = True
-
+        #False if we've gotten a game over
+        
+        self.key_actions = {
+                            pygame.K_SPACE : self.ship.on_fire_bullet ,
+                            pygame.K_F1    : config.toggle_fullscreen ,
+                            pygame.K_c     : self.clear_blocks        ,
+                            pygame.K_d     : config.toggle_debug      ,
+                            pygame.K_f     : config.toggle_frame_limit,
+                            pygame.K_p     : config.toggle_pause      ,
+                            pygame.K_u     : self.add_ufo             ,
+                            }
+        #The keys available for use; key is keycode, element is a function
+        
+        self.mouse_actions = {
+                              1: color.RED   ,
+                              2: color.YELLOW,
+                              3: color.BLUE  ,
+                             }
+        #What clicking the mouse can do: key is mouse button, element is color
         
         PLAYER.add(self.ship)
         HUD.add(self.hud_score, self.hud_lives)
         enemysquadron.reset()
         
+    def __del__(self):
+        for g in self.group_list:
+            g.empty()
     
-    def events(self,states):
-        for e in states:
-            if e.type == pygame.MOUSEBUTTONDOWN:
+    def events(self, events):
+        for e in events:
+        #For all events passed in...
+            if e.type == pygame.MOUSEBUTTONDOWN and config.debug:
             #If a mouse button is clicked...
-                if   e.button == 1:
-                #If it was the left button...
-                    BLOCKS.add(block.Block((e.pos[0], 0), color.RED))
-                elif e.button == 2:
-                #If it was the middle button...
-                    BLOCKS.add(block.Block((e.pos[0], 0), color.YELLOW))
-                elif e.button == 3:
-                #If it was the right button...
-                    BLOCKS.add(block.Block((e.pos[0], 0), color.BLUE))
-            elif e.type == pygame.KEYDOWN:
+                self.make_block(e.pos[0], self.mouse_actions[e.button])
+            elif e.type == pygame.KEYDOWN and e.key in self.key_actions:
             #If a key is pressed...
-                if e.key == pygame.K_SPACE:
-                #If the space bar is pressed...
-                    self.ship.on_fire_bullet()
-                elif e.key == pygame.K_f:
-                #If the F key is pressed...
-                    self.frame_limit = not self.frame_limit
-                elif e.key == pygame.K_u:
-                #If the U key is pressed...
-                    if self.ufo.state == ufo.UFO.STATES.IDLE:
-                        ENEMIES.add(self.ufo)
-                        self.ufo.state = ufo.UFO.STATES.APPEARING
-                elif e.key == pygame.K_c:
-                    blockgrid.clear()
-                elif e.key == pygame.K_p:
-                    config.togglePause()
-                     
+                self.key_actions[e.key]()  
     
     def logic(self):
         self.collision_grid.update()
@@ -98,7 +103,7 @@ class InGameState(gamestate.GameState):
         if len(ENEMIES) == 0:
         #If all enemies have been killed...
             enemysquadron.reset()
-            enemy.Enemy.velocity[0] += math.copysign(.05, enemy.Enemy.velocity[0])
+            enemy.Enemy.velocity[0] = abs(enemy.Enemy.velocity[0]) + 0.05
             
         if enemy.Enemy.should_flip:
         #If at least one enemy has touched the side of the screen...
@@ -107,6 +112,7 @@ class InGameState(gamestate.GameState):
             enemy.Enemy.should_flip = False
             
         if lives == 0:
+        #If we run out of lives...
             self.game_running = False
             
         if not self.game_running:
@@ -121,11 +127,13 @@ class InGameState(gamestate.GameState):
         pygame.display.get_surface().blit(config.BG, (0, 0))
         
         if score != prev_score:
-            self.hud_score.image = config.FONT.render("Score: " + str(score), False, color.WHITE).convert(config.DEPTH, config.FLAGS)
+        #If our score has changed since the last frame...
+            self.hud_score.image = config.FONT.render("Score: %i" % score, False, color.WHITE).convert(config.DEPTH, config.FLAGS)
             prev_score = score
             
         if lives != prev_lives:
-            self.hud_lives.image = config.FONT.render("Lives: " + str(lives), False, color.WHITE).convert(config.DEPTH, config.FLAGS)
+        #If our lives have changed since the last frame...
+            self.hud_lives.image = config.FONT.render("Lives: %i" % lives, False, color.WHITE).convert(config.DEPTH, config.FLAGS)
             prev_lives = lives
         
         for g in self.group_list:
@@ -133,16 +141,30 @@ class InGameState(gamestate.GameState):
             pygame.display.update(g.draw(pygame.display.get_surface()))
             
         pygame.display.flip()
-        pygame.display.set_caption("Score: " + str(score) + "    FPS: " + str(round(self.fpsTimer.get_fps(), 3)))
+        pygame.display.set_caption("Score: %i    FPS: %f" % (score, round(self.fpsTimer.get_fps(), 3)))
             
-        self.fpsTimer.tick(60 * self.frame_limit)
+        self.fpsTimer.tick(60 * config.limit_frame)
 
+
+    def add_ufo(self):
+        if config.debug and self.ufo.state == ufo.UFO.STATES.IDLE:
+        #If we're in debug mode and the UFO is not active...
+            ENEMIES.add(self.ufo)
+            self.ufo.state = ufo.UFO.STATES.APPEARING
+            
+    def make_block(self, pos, c):
+        BLOCKS.add(block.Block((pos, 0), c))
+        
+    def clear_blocks(self):
+        if config.debug:
+        #If we're in debug mode...
+            blockgrid.blocks.clear()
 
     def game_over(self):
         enemy.Enemy.velocity = [0, 0]
-        gameovertext = hudobject.HudObject()
-        gameovertext.image = config.FONT.render("GAME OVER", False, color.Colors.WHITE).convert(config.DEPTH, config.FLAGS)
-        gameovertext.rect = pygame.Rect(config.SCREEN_WIDTH/2, config.SCREEN_HEIGHT/2, 0, 0)
-        self.ship.state = player.STATES.DYING
+        gameovertext         = hudobject.HudObject(config.FONT.render("GAME OVER", False, color.WHITE).convert(config.DEPTH, config.FLAGS),
+                                                   pygame.Rect(config.SCREEN_WIDTH/2, config.SCREEN_HEIGHT/2, 0, 0)
+                                                   )
+        self.ship.state      = player.STATES.DYING
         HUD.add(gameovertext)
 
