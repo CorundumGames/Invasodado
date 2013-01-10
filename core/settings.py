b05dc90913cd4bfcb2f16b7a63a3aa8b578d9e32
Kyle O'Connor <koconnor26@gmail.com>
2013-01-10 15:10:09 -0500

    Fixed crlf issue

--- core/settings.py
+++
@@ -1,9 +0,0 @@
-'''This module provides access to the configurations made in the game's Options
-menu.  OBJECTS IN ALL CAPS are constants.
-'''
-
-#The current screen settings.
-resolution = (640, 480)
-
-#Whether we're currently in fullscreen mode.
-fullscreen = False
--- core/vartracker.py
+++
@@ -1,6 +0,0 @@
-class VarTracker:
-    def __init__(self):
-        pass
-    
-    def __del__(self):
-        pass
