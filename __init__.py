ac3cb058750b1ceb6f41977f4409f0cb428815bf
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2013-02-16 15:43:36 -0500

    Removed profile.py, moved its functionality into __init__.py
    -To profile Invasodado, run the game with 'profile' on the command line

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ Invasodado is a mix between Space Invaders and Columns.
 
 Copyright 2012-2013 Corundum Games
 '''
-
+from sys import argv
 
 import pygame.display
 import pygame.event
@@ -18,6 +18,11 @@ pygame.font.init()
 from core import gsm
 
 def main():
+    '''
+    Meant to be executed only once.  main() exists to facilitate profiling, as
+    cProfile wants a function, not a module.
+    '''
+    
     while True:
     #Until the game is closed...
         gsm.update()
@@ -25,6 +30,23 @@ def main():
         if pygame.event.peek(pygame.QUIT):
         #If we've received a request to quit (either by the user or the OS)...
             return
+        
+def profile():
+    '''
+    Call this function if we're profiling the game.
+    '''
+    import cProfile
+    import pstats
+        
+    cProfile.run('main()', 'profile.out')
+    data = pstats.Stats('profile.out')
+    data.sort_stats('cumulative')
+    data.print_stats()
 
 if __name__ == '__main__':
-    main()
+    #If this script is being executed directly...
+    if 'profile' in argv:
+    #If we're not profiling the game...
+        profile()
+    else:
+        main()
--- profile.py
+++
@@ -1,8 +0,0 @@
-import cProfile
-import pstats
-import __init__
-
-cProfile.run('__init__.main()', 'profile.out')
-p = pstats.Stats('profile.out')
-p.sort_stats('cumulative')
-p.print_stats()
