f6906f0358af4d8a41c87b74089fb0cbc5a466db
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2013-02-17 18:43:22 -0500

    Normalized the grid representation of blocks
    -I.e. blocks are now references block[x][y] instead of block[row][col] (which was essentially block[y][x])
    -Removed blockgrid._match_set and made it local to blockgrid.update()
    -Made Block.GRAVITY and Block.MAX_SPEED fully global

--- game/block.py
+++ game/block.py
@@ -11,8 +11,9 @@ from game            import blockgrid
 from blockgrid       import blocks
 
 
-FRAMES  = [pygame.Rect(32 * i, 160, 32, 32) for i in range(8)]
-GRAVITY = 0.5
+FRAMES    = [pygame.Rect(32 * i, 160, 32, 32) for i in range(8)]
+GRAVITY   = 0.5
+MAX_SPEED = 12.0
 
 _blocks_set = set()
 _bump       = pygame.mixer.Sound(os.path.join('sfx', '_bump.wav'))
@@ -79,8 +80,7 @@ class Block(GameObject):
     collisions     = None
     GROUP          = None
     particle_group = None
-    GRAVITY        = 0.5
-    MAX_SPEED      = 12.0
+    
     STATES         = config.Enum('IDLE', 'APPEARING', 'ACTIVE', 'START_FALLING', 'FALLING', 'IMPACT', 'DYING')
 
     def __init__(self, position, newcolor=choice(color.LIST), special=False):
@@ -88,68 +88,56 @@ class Block(GameObject):
         self._anim    = 0
         self.color    = newcolor
         self.image    = _block_frames[id(self.color)][0]
-        self.position = position
-
-        size = self.image.get_size()
-        self.rect     = pygame.Rect(
-                                    [
-                                     round(position[0] / size[0]) * size[0],
-                                     round(position[1] / size[1]) * size[1]
-                                    ],
-                                    size
-                                   )
+        self.position = self.__get_snap()
+        self.rect     = pygame.Rect(self.position, self.image.get_size()) #(x, y)
         self._target  = None
         self._special = special
         self.state    = Block.STATES.IDLE
 
     def __str__(self):
-        return ("Block of color %s at cell %s at position %s with target %s" %
-               (self.color, self.gridcell, self.position, self._target))
+        return ("<Block with color: %s, cell: %s, position: %s, rect: %s, target: %s, state: %i>" %
+               (self.color, self.gridcell, self.position, self.rect, self._target, self.state))
+        
+    def __repr__(self):
+        return self.__str__()
 
     def appear(self):
-        self.image    = _block_frames[id(self.color)][0]
-
-        size = self.image.get_size()
-        self.position     = [
-                             round(self.position[0] / size[0]) * size[0],
-                             round(self.position[1] / size[1]) * size[1]
-                            ]
+        self.image        = _block_frames[id(self.color)][0]
+        self.position     = self.__get_snap()
         self.rect.topleft = self.position
         self.gridcell     = [
-                             self.rect.centery / self.rect.height, 
-                             self.rect.centerx / self.rect.width
-                            ]  #(row, column)
-        self._target      = blockgrid.SIZE[0] - 1
+                             self.rect.centerx / self.rect.width,
+                             self.rect.centery / self.rect.height,
+                            ] #(x, y)
+        self._target      = self.__set_target()
         self.emitter      = ParticleEmitter(_block_particles[id(self.color)], self.rect, 5, Block.particle_group)
         self.state        = Block.STATES.START_FALLING
         self.add(Block.GROUP)
 
     def start_falling(self):
-        gridcell = self.gridcell
-
-        self.acceleration[1] = Block.GRAVITY
+        '''
+        Starts the Block falling down.  Only called once before this Block's
+        state switches to STATES.FALLING.  Blocks that are falling must not be
+        part of matches.
+        '''
+        self.acceleration[1] = GRAVITY
         blockgrid.check_block(self, False)
-        for i in xrange(gridcell[0] + 1, blockgrid.SIZE[0]):
-        #For all grid cells below this one...
-            other = blocks[i][gridcell[1]]
-            if other and other.state in {Block.STATES.ACTIVE, Block.STATES.IMPACT}:
-            #If this grid cell is occupied...
-                self._target = i - 1
-                break
+        self.__set_target()
 
-        block_above = blocks[gridcell[0] - 1][gridcell[1]]
-        if gridcell[0] and block_above:
-        #If there's at least one block above us...
-            assert isinstance(block_above, Block), \
-            "%s expected a Block, got a %s" % (self, block_above)
+        if self.gridcell[1]:
+        #If we're not at the top of the grid...
+            block_above = blocks[self.gridcell[0]][self.gridcell[1] - 1]
+            if block_above:
+            #If there's at least one block above us...
+                assert isinstance(block_above, Block), \
+                "%s expected a Block, got a %s" % (self, block_above)
 
-            for i in reversed(blocks):
-            #For all grid cells above us...
-                if i[gridcell[1]]:
-                #If this is actually a block...
-                    i[gridcell[1]].state = Block.STATES.START_FALLING
-                else:
-                    break
+                for i in blocks[self.gridcell[0]]:
+                #For all grid cells above us...
+                    if i and i.state == Block.STATES.ACTIVE:
+                        i.state = Block.STATES.START_FALLING
+                    else:
+                        break
 
         self.state = Block.STATES.FALLING
 
@@ -163,10 +151,10 @@ class Block(GameObject):
         position = self.position
         rect     = self.rect
         
-        self.velocity[1] = min(Block.MAX_SPEED, self.velocity[1] + self.acceleration[1])
+        self.velocity[1] = min(MAX_SPEED, self.velocity[1] + self.acceleration[1])
         position[1]     += self.velocity[1]
         rect.top         = position[1] + 0.5 #Round to the nearest integer
-        gridcell[0]      = rect.centery / rect.height
+        gridcell[1]      = self.rect.centery / self.rect.height
         self.emitter.rect.topleft = rect.topleft
 
         if self._anim < len(FRAMES) - 1:
@@ -175,31 +163,25 @@ class Block(GameObject):
             self.image  = _block_frames[id(self.color)][self._anim]
 
         if self._special:
-        #If this is a _special block...
+        #If this is a special block...
             self.image = choice(_block_frames.values())[self._anim]
 
-        for i in xrange(gridcell[0] + 1, blockgrid.SIZE[0]):
-        #For all grid cells below this one...
-            other = blocks[i][gridcell[1]]
-            if other and other.state in {Block.STATES.ACTIVE, Block.STATES.IMPACT}:
-            #If this grid cell is occupied...
-                self._target = i - 1
-                break
+        self.__set_target()
 
         if self._target is not None and self._target < 1:
         #If we go past the top of the screen...
             Block.block_full = True
         
-        if gridcell[0] >= blockgrid.SIZE[0] - 1:
+        if rect.bottom >= blockgrid.RECT.bottom:
         #If we've hit the bottom of the grid...
-            rect.bottom = blockgrid.RECT.bottom
-            position[1] = rect.topleft[1]
-            self.state  = Block.STATES.IMPACT
-        elif blocks[gridcell[0] + 1][gridcell[1]]:
+            rect.bottom   = blockgrid.RECT.bottom
+            position[1]   = rect.top
+            self.state    = Block.STATES.IMPACT
+        elif self.gridcell[1] + 1 < blockgrid.SIZE[1] and blocks[gridcell[0]][gridcell[1] + 1]:
         #Else if it was another block...
-            below       = blocks[gridcell[0] + 1][gridcell[1]]
+            below       = blocks[gridcell[0]][gridcell[1] + 1]
             rect.bottom = below.rect.top
-            position[1] = rect.topleft[1]
+            position[1] = rect.top
             self.state  = Block.STATES.IMPACT
             assert isinstance(below, Block) or below is None, \
             "A %s is trying to collide with a stray %s!" % (self, below)
@@ -215,56 +197,90 @@ class Block(GameObject):
 
         if self.rect.bottom < blockgrid.RECT.bottom:
         #If we're not at the bottom of the grid...
-            block_below = blocks[gridcell[0] + 1][gridcell[1]]
+            block_below = blocks[gridcell[0]][gridcell[1] + 1]
             if not block_below or block_below.state not in {Block.STATES.ACTIVE, Block.STATES.IMPACT}:
             #If there's no block directly below...
                 blockgrid.check_block(self, False)
                 self.acceleration[1] = GRAVITY
                 self.state           = Block.STATES.START_FALLING
+                
+        if __debug__ and self.rect.collidepoint(pygame.mouse.get_pos()):
+            print self
 
     def stop(self):
         '''
         Handles the Block when it hits the bottom of the grid or another block.
         Changes velocity, plays sounds, etc.
         '''
-        gridcell = self.gridcell
-
         self.acceleration[1] = 0.0
         self.velocity[1]     = 0.0
-        gridcell[0]          = self.rect.centery / self.rect.height #(row, col)
+        self.position        = self.__get_snap()
+        self.rect.topleft    = self.position
+        self.gridcell[1]     = self.rect.centery / self.rect.height #(row, col)
         self._target         = None
-        print blocks[gridcell[0]][gridcell[1]]
-        blocks[gridcell[0]][gridcell[1]] = self
-        print blocks[gridcell[0]][gridcell[1]]
+        blocks[self.gridcell[0]][self.gridcell[1]] = self
 
-        blockgrid.check_block(self)
+        blockgrid.check_block(self, True)
         _bump.play()
         blockgrid.update()
 
         if self._special:
         #If this is a special block...
-            if gridcell[0] < blockgrid.SIZE[0] - 1:
+            if self.gridcell[1] < blockgrid.SIZE[1] - 1:
             #If we're not at the bottom of the grid...
-                self.color = blocks[gridcell[0]+1][gridcell[1]].color
+                self.color = blocks[self.gridcell[0]][self.gridcell[1]+ 1].color
                 blockgrid.clear_color(self.color)
             else:
-                blockgrid.clear_row(gridcell[0])
+                blockgrid.clear_row(self.gridcell[1])
                 
         self.state = Block.STATES.ACTIVE
 
     def vanish(self):
-        self.emitter.burst(20)
-        self._anim                                 = 0
-        self.kill()
         blockgrid.check_block(self, False)
+        self.emitter.burst(20)
+        self.kill()
+        
+        blocks[self.gridcell[0]][self.gridcell[1]] = None
+        self._anim                                 = 0
         self.position                              = [-300.0, -300.0]
         self.rect.topleft                          = self.position
-        blocks[self.gridcell[0]][self.gridcell[1]] = None
         self.gridcell                              = None
         self._target                               = None
         self.state                                 = Block.STATES.IDLE
+        self.__replace()
+        
+    def __replace(self):
+        '''
+        Puts this Block back in the set of spares.
+        
+        @postcondition: This Block is ready to be recycled.
+        '''
         _blocks_set.add(self)
-
+        
+    def __set_target(self):
+        '''
+        Determines which row this block should fall down to.
+        '''
+        for i in xrange(self.gridcell[1] + 1, blockgrid.SIZE[1]):
+        #For all grid cells below this one...
+            other = blocks[self.gridcell[0]][i]
+            if other and other.state in {Block.STATES.ACTIVE, Block.STATES.IMPACT}:
+            #If this grid cell is occupied...
+                self._target = i - 1
+                break
+        
+        return self._target
+    
+    def __get_snap(self):
+        '''
+        Returns a position list that snaps this block to the grid.
+        '''
+        size = self.image.get_size()
+        return [
+                round(self.position[0] / size[0]) * size[0],
+                round(self.position[1] / size[1]) * size[1]
+               ]
+        
     actions = {
                 STATES.IDLE         : None           ,
                 STATES.APPEARING    : 'appear'       ,
--- game/blockgrid.py
+++ game/blockgrid.py
@@ -4,7 +4,6 @@ blocks.  It's designed around Invasodado, but with a little work it could be
 refitted for other match-3 games like Bejewelled or Puzzle League.
 '''
 
-#from itertools import chain
 from os.path   import join
 
 from pygame.display import get_surface
@@ -13,16 +12,21 @@ import pygame.mixer
 
 from game import gamedata
 
-BLOCK_TYPE       = None
-CELL_SIZE = (32, 32)
-SIZE      = (12, 20) #(row, column)
-RECT      = pygame.rect.Rect((0, 0), (get_surface().get_width(), SIZE[0] * CELL_SIZE[0]))
+def get_empty_block_array():
+    '''
+    Returns an empty 2D array to reset the block grid
+    '''
+    return [[None for i in xrange(SIZE[1])] for j in xrange(SIZE[0])]
 
-blocks           = [[None for i in xrange(SIZE[1])] for j in xrange(SIZE[0])]
-_blocks_to_check  = set()
+BLOCK_TYPE = None
+CELL_SIZE  = (32, 32)
+SIZE       = (20, 12) #(width, height)
+RECT       = pygame.rect.Rect((0, 0), (get_surface().get_width(), SIZE[1] * CELL_SIZE[1]))
+
+blocks           = get_empty_block_array()
+_blocks_to_check = set()
 _blocks_to_clear = set()
 _block_clear     = pygame.mixer.Sound(join('sfx', 'clear.wav'))
-_match_set       = set()
 
 def check_block(block, should_check=True):
     if should_check:
@@ -38,11 +42,10 @@ def clean_up():
     @postcondition: No more objects of BLOCK_TYPE exist in memory.
     '''
     global blocks
-    blocks = [[None for i in xrange(SIZE[1])] for j in xrange(SIZE[0])]
+    blocks = get_empty_block_array()
     _blocks_to_check.clear()
     _blocks_to_clear.clear()
-    _match_set.clear()
-
+    
 def clear_color(color):
     '''
     Destroys all blocks of the specified color.
@@ -58,12 +61,12 @@ def clear_row(row):
     '''
     Destroys all blocks in the specified row.
 
-    @param row: The row of blocks to destroy, on [0, SIZE[0]).
+    @param row: The row of blocks to destroy, on [0, SIZE[1]).
     @postcondition: No blocks on-screen exist in the given row.
     '''
 
-    assert 0 <= row < SIZE[0], \
-    "Expected row value in 0 <= x < 20, but got %i" % row
+    assert 0 <= row < SIZE[1], \
+    "Expected row value in 0 <= x < %i, but got %i" % (SIZE[1], row)
 
     for i in (j for j in blocks[row] if j):
     #For all blocks in this row...
@@ -76,7 +79,7 @@ def update():
     @postcondition: Sets of 3+ like-colored blocks are removed from the game.
     '''
     global blocks
-    blocks = [[None for i in xrange(SIZE[1])] for j in xrange(SIZE[0])]
+    blocks = get_empty_block_array()
 
     for i in (j for j in BLOCK_TYPE.GROUP if j and j.state == BLOCK_TYPE.STATES.ACTIVE):
     #For all blocks that are on the grid and not moving...
@@ -85,24 +88,24 @@ def update():
     
     for b in _blocks_to_check:
     #For all blocks to check for matches...
-        _match_set.add(b)    #Start with a match of one
+        match_set = {b}  #Start with a match of one
         next_blocks = ([], [], [], []) #Respectively holds blocks down, down-right, right, up-right
         for j in (1, 2):
-            if b.gridcell[0] + j < len(blocks):
+            if b.gridcell[1] + j < SIZE[1]:
             #If we're not out of bounds...
-                next_blocks[0].append(blocks[b.gridcell[0]+j][ b.gridcell[1]])
+                next_blocks[0].append(blocks[b.gridcell[0]][b.gridcell[1] + j])
                 #add the block below
-            if b.gridcell[0] + j < len(blocks) and b.gridcell[1] + j < len(blocks[0]):
+            if b.gridcell[0] + j < SIZE[0] and b.gridcell[1] + j < SIZE[1]:
             #If we're not out of bounds...
-                next_blocks[1].append(blocks[b.gridcell[0]+j][b.gridcell[1]+j])
+                next_blocks[1].append(blocks[b.gridcell[0] + j][b.gridcell[1] + j])
                 #add the block down and to the right
-            if b.gridcell[1] + j < len(blocks[0]):
+            if b.gridcell[0] + j < SIZE[0]:
             #If we're not out of bounds...
-                next_blocks[2].append(blocks[b.gridcell[0]][b.gridcell[1]+j])
+                next_blocks[2].append(blocks[b.gridcell[0] + j][b.gridcell[1]])
                 #add the block to the right
-            if b.gridcell[0] - j >= 0 and b.gridcell[1] + j < len(blocks[0]):
+            if b.gridcell[1] - j >= 0 and b.gridcell[0] + j < SIZE[0]:
             #If we're not out of bounds...
-                next_blocks[3].append(blocks[b.gridcell[0]-j][b.gridcell[1]+j])
+                next_blocks[3].append(blocks[b.gridcell[0]+j][b.gridcell[1]-j])
                 #add the block up and to the right
         #TODO: Optimize this so the above mess is done in one loop, if possible
 
@@ -110,16 +113,14 @@ def update():
         #For all the lists of blocks above...
             if len([1 for j in i if j and i is not j and b.color is j.color]) >= 2:
             #If there are 2+ blocks that are the same color as the first...
-                _match_set.update(i) #Mark these blocks as matching
+                match_set.update(i) #Mark these blocks as matching
 
-        if len(_match_set) >= 3:
+        if len(match_set) >= 3:
         #If at least 3 blocks are aligned...
-            _blocks_to_clear.update(_match_set) #Mark these blocks for removal
-            gamedata.score     += (len(_match_set) ** 2) * gamedata.multiplier
+            _blocks_to_clear.update(match_set) #Mark these blocks for removal
+            gamedata.score     += (len(match_set) ** 2) * gamedata.multiplier
             gamedata.combo      = True
             gamedata.multiplier = gamedata.DEFAULT_MULTIPLIER * 2
-
-        _match_set.clear()
 
     if len(_blocks_to_clear) >= 3:
     #If we're clearing any blocks...
