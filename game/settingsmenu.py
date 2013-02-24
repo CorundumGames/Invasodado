97a081b16fd6b594b93b6662a528cf79bbff1b62
Kyle O'Connor <koconnor26@gmail.com>
2013-02-24 15:48:57 -0500

    Added more settings
    -Volume and difficulty can now be changed in settings menu.
    -They don't do anything yet.

--- game/settingsmenu.py
+++ game/settingsmenu.py
@@ -31,9 +31,9 @@ DIST_APART_STATUS = 320
 
 MENU_CORNER    = (32, 64)
 #The location of the top-left corner of the menu
-SETTINGS_FIELDS    = 'fullscreen colorblind difficulty back'
+SETTINGS_FIELDS    = 'fullscreen colorblind difficulty musicvolume effectsvolume back'
 SETTINGS_KEYS      = namedtuple('Settings', SETTINGS_FIELDS)
-SETTINGS_NAMES = ("Full-Screen", "Colorblind Mode", "Difficulty", "Back")
+SETTINGS_NAMES = ("Full-Screen", "Colorblind Mode", "Difficulty", "Music Volume", "Effects Volume", "Back")
 TITLE_LOCATION = (config.SCREEN_RECT.centerx - 64, 32)
 ################################################################################
 
@@ -51,7 +51,8 @@ class SettingsMenu(GameState):
         a = make_text(SETTINGS_NAMES, pos=MENU_CORNER, vspace=DIST_APART)
 
         b = make_text(
-                      [config.on_off(settings.fullscreen), config.on_off(settings.color_blind), "Easy", ""],
+                      [config.on_off(settings.fullscreen), config.on_off(settings.color_blind), config.difficulty_string(), str(config._current_music_volume),
+                        str(config._current_effects_volume), ""],
                       pos=[MENU_CORNER[0] + DIST_APART_STATUS, MENU_CORNER[1]],
                       vspace=DIST_APART
                      )
@@ -61,16 +62,20 @@ class SettingsMenu(GameState):
 
 
         self.menu_actions = (
-                             self.__toggle_fullscreen      ,
-                             self.__toggle_color_blind_mode,
-                             self.__toggle_difficulty      ,
+                             self.__toggle_fullscreen            ,
+                             self.__toggle_color_blind_mode      ,
+                             self.__toggle_difficulty            ,
+                             self.__toggle_music_volume          ,
+                             self.__toggle_effects_volume        ,
                              partial(self.change_state, MainMenu),
                             )
 
         self.key_actions  = {
-                             pygame.K_RETURN: self.__enter_selection         ,
-                             pygame.K_UP    : partial(self.__move_cursor, -1),
-                             pygame.K_DOWN  : partial(self.__move_cursor,  1),
+                             pygame.K_RETURN: partial(self.__enter_selection, 1)  ,
+                             pygame.K_LEFT  : partial(self.__enter_selection, -1) ,
+                             pygame.K_RIGHT : partial(self.__enter_selection, 1)  ,
+                             pygame.K_UP    : partial(self.__move_cursor, -1)     ,
+                             pygame.K_DOWN  : partial(self.__move_cursor,  1)     ,
                              pygame.K_ESCAPE: partial(self.change_state, MainMenu),
                             }
 
@@ -82,9 +87,12 @@ class SettingsMenu(GameState):
         self.hud_cursor.rect.midright = self.menu[self.cursor_index][0].rect.midleft
         super().render()
 
-    def __enter_selection(self):
+    def __enter_selection(self, toggle):
         '''Go with the cursor_index the player made.'''
-        self.menu_actions[self.cursor_index]()
+        if(self.cursor_index != len(self.menu_actions) - 1):
+            self.menu_actions[self.cursor_index](toggle)
+        else:
+            self.menu_actions[self.cursor_index]()
 
     def __move_cursor(self, index):
         '''Move the cursor.'''
@@ -93,14 +101,26 @@ class SettingsMenu(GameState):
         self.cursor_index %= len(self.menu)
         config.CURSOR_BEEP.play()
 
-    def __toggle_fullscreen(self):
+    def __toggle_fullscreen(self, toggle):
+        #toggle doesn't really matter because it's the same both ways
         config.toggle_fullscreen()
         self.menu.fullscreen[1].image = HudObject.make_text(config.on_off(settings.fullscreen), surfaces=True)
 
-    def __toggle_color_blind_mode(self):
+    def __toggle_color_blind_mode(self, toggle):
+        #toggle doesn't really matter because it's the same both ways
         config.toggle_color_blind_mode();
         self.menu.colorblind[1].image = HudObject.make_text(config.on_off(settings.color_blind), surfaces=True)
 
-    def __toggle_difficulty(self):
-        pass
-    
+    def __toggle_difficulty(self, toggle):
+        config.toggle_difficulty(toggle)
+        self.menu.difficulty[1].image = HudObject.make_text(config.difficulty_string(), surfaces=True)
+
+    def __toggle_music_volume(self, delta_volume):
+        delta_volume *= 10
+        config.toggle_music_volume(delta_volume)
+        self.menu.musicvolume[1].image = HudObject.make_text(str(config._current_music_volume), surfaces=True)
+        
+    def __toggle_effects_volume(self, delta_volume):
+        delta_volume *= 10
+        config.toggle_effects_volume(delta_volume)
+        self.menu.effectsvolume[1].image = HudObject.make_text(str(config._current_effects_volume), surfaces=True)
